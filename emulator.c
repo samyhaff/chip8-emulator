84e588408b4067178c3065154f9dc4b58ef43e98
samyhaff <haffoudhisamy@gmail.com>
2021-02-09 23:05:32 +0100

    added a lot of opcodes

--- emulator.c
+++ emulator.c
@@ -1,17 +1,25 @@
+/* 
+TODO get key presses
+TODO set speed to 60Hz
+TODO SDL 
+TODO add all opcodes
+TODO BEEP
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 
-unsigned short opcode = 0;
+unsigned short opcode;
 unsigned char memory[4096]; 
 unsigned char V[16];
 unsigned short I = 0;
-unsigned short pc = 0x200;
+unsigned short pc = 0x200; // program starts at location 0x200
 unsigned char gfx[64 * 32];
 unsigned char delay_timer = 0;
 unsigned char sound_timer = 0;
 unsigned short stack[16];
-unsigned short sp = 0;
+unsigned char sp = 0;
 unsigned char key[16];
 unsigned char drawFlag = 0;
 unsigned char fontset[80] = 
@@ -34,91 +42,154 @@ unsigned char fontset[80] =
   0xF0, 0x80, 0xF0, 0x80, 0x80  // F
 };
 
-void initialize() {
-    for (int i = 0; i < 64 * 32; i++)
-        gfx[i] = 0;
-    for (int i = 0; i < 16; i++) {
-        stack[i] = 0;
-        V[i] = 0;
-    }
-    for (int i = 0; i < 4096; i++) 
-        memory[i] = 0;
-    for (int i = 0; i < 80; i++)
-        memory[i] = fontset[i];
-}
-
 void emulateCycle() {
     opcode = memory[pc] << 8 | memory[pc + 1];
 
     switch (opcode & 0xf000) {
+        case 0x0000:
+            switch (opcode & 0x000f) {
+                case 0x0000:
+                    // clear the display
+                    pc += 2;
+                    break;
+                case 0x000e:
+                    sp--;
+                    pc = stack[sp];
+                    break;
+            }
+            break;
+        case 0x1000:
+            pc = opcode & 0x0fff;
+            break;
+        case 0x2000:
+            stack[sp] = pc;
+            sp++;
+            pc = opcode & 0x0fff;
+            break;
+        case 0x3000:
+           if (V[(opcode & 0x0f00) >> 8]  == opcode & 0x00ff)
+               pc += 2;
+            pc += 2;
+            break;
+        case 0x4000:
+           if (V[(opc=de & 0x0f00) >> 8]  != opcode & 0x00ff)
+               pc += 2;
+            pc += 2;
+            break;
+        case 0x5000:
+            if (V[(opcode & 0x0f00) >> 8] == V[(opcode & 0x00f0) > 4]) 
+                pc += 2;
+            pc += 2;
+            break;
+        case 0x6000:
+            V[(opcode & 0x0f00) > 8] = opcode & 0x00ff;
+            pc += 2;
+            break;
+        case 0x7000:
+            V[(opcode & 0x0f00) > 8] += opcode & 0x00ff;
+            pc += 2;
+            break;
+        case 0x8000:
+            switch (opcode & 0x000f) {
+                case 0x0000:
+                    V[(opcode & 0x0f00) >> 8] = V[(opcode & 0x00f0) >> 4];
+                    pc += 2;
+                    break;
+                case 0x0001:
+                    V[(opcode & 0x0f00) >> 8] |= V[(opcode & 0x00f0) >> 4];
+                    pc += 2;
+                    break;
+                case 0x0002:
+                    V[(opcode & 0x0f00) >> 8] &= V[(opcode & 0x00f0) >> 4];
+                    pc += 2;
+                    break;
+                case 0x0003:
+                    V[(opcode & 0x0f00) >> 8] ^= V[(opcode & 0x00f0) >> 4];
+                    pc += 2;
+                    break;
+                case 0x0004:
+                    unsigned short result = V[(opcode & 0x0f00) >> 8] + V[(opcode & 0x00f0) >> 4];
+                    if (result > 255)
+                        V[0xf] = 1;
+                    else 
+                        V[0xf] = 0;
+                    V[(opcode & 0x0f00) >> 8] = result && 0x00ff;
+                    pc += 2;
+                    break;
+                case 0x0005:
+                    if V[(opcode & 0x0f00) >> 8] > V[(opcode & 0x00f0) >> 4]
+                        V[0xf] = 1;
+                    else
+                        V[0xf] = 0;
+                    V[(opcode & 0x0f00) >> 8] -= V[(opcode & 0x00f0) >> 4];
+                    pc += 2;
+                    break;
+                case 0x0006:
+                    if (V[(opcode & 0x0f00) >> 8] & 0x0001 == 1) 
+                        V[0xf] = 1;
+                    else
+                        V[0xf] = 0;
+                    V[(opcode & 0x0f00) >> 8] /= 2;
+                    pc += 2;
+                    break;
+                case 0x0007:
+                    if V[(opcode & 0x0f00) >> 8] < V[(opcode & 0x00f0) >> 4]
+                        V[0xf] = 1;
+                    else
+                        V[0xf] = 0;
+                    V[(opcode & 0x0f00) >> 8] = V[(opcode & 0x00f0) >> 4] - V[(opcode & 0x0f00) >> 8];
+                    pc += 2;
+                    break;
+                case 0x000e:
+                    if (V[(opcode & 0x8000) >> 15] == 1)
+                        V[0xf] = 1;
+                    else
+                        V[0xf] = 0;
+                    V[(opcode & 0x0f00) >> 8] *= 2;
+                    pc += 2;
+                    break;
+            }
+            break;
+        case 0x9000:
+            if !(V[(opcode & 0x0f00) >> 8] == V[(opcode & 0x00f0) >> 4])
+                pc += 2;
+            pc += 2;
+            break;
         case 0xa000:
             I = opcode & 0x0fff;
             pc += 2;
-        break;
-        case 0x2000:
-            stack[sp] = pc;
-            ++sp;
-            pc = opcode & 0x0fff;
-        break;
-        case 0x0004:
-            if (V[(opcode & 0x00f0) >> 4] > (0xff - V[(opcode & 0x0f00) >> 8]))
-                V[0xf] = 1; // carry
-            else
-                V[0xf] = 0;
-            V[(opcode & 0x0f00) >> 8] += V[(opcode & 0x00f0) >> 4];
+            break;
+        case 0xb000:
+            pc = V[0] + (opcode & 0x0fff);
+        case 0xc000:
+            unsigned char r = rand() % 256;            
+            V[(opcode & 0x0f00) >> 8] = (opcode & 0x00ff) & r;
             pc += 2;
-        break;
-        case 0x0033:
-            memory[I] = V[(opcode & 0x0f00) >> 8] / 100;
-            memory[I + 1] = (V[(opcode & 0x0f00) >> 8] / 10) % 10;
-            memory[I + 2] = (V[(opcode & 0x0f0) >> 8] % 100) % 10;
+            break;
+        case 0xd000:
+            // affichage
             pc += 2;
-        break;
-        // ...
-        case 0xd000: {
-            unsigned short x = V[(opcode & 0x0f00) >> 8];
-            unsigned short y = V[(opcode & 0x00f0) >> 4];
-            unsigned short height = opcode & 0x000f;
-            unsigned short pixel;
-            V[0xf] = 0;
-            for (int yline = 0; yline < height; yline++) {
-                pixel = memory[I + yline];
-                for (int xline = 0; xline < 8; xline++) {
-                    if ((pixel & (0x80 >> xline)) != 0) {
-                        if (gfx[(x + xline + ((y + yline) * 64))] == 1)
-                            V[0xf] = 1;
-                        gfx[x + xline + ((y + yline) * 64)] ^= 1;
-                    }
-                }
-            }
-            drawFlag = 1;
-            pc += 2;
-        }
-        break;
+            break; 
         case 0xe000:
-            switch (opcode & 0x00ff) {
-                case 0x009e: 
-                    if (key[V[(opcode & 0x0f00) >> 8]] != 0)
-                        pc += 4;
-                    else
-                        pc += 2;
-                break;
+            switch (opcode & 0x000f) {
+                case 0x000e:
+                    // key
+                    break;
+                case 0x0001:
+                    // key
+                    break;
             }
-        break;
-        default:
-            printf("Unknow opcode 0x%X\n", opcode);
-    }
-
-    if (delay_timer > 0)
-        --delay_timer;
-    if (sound_timer > 0) {
-        if (sound_timer == 1)
-            printf("BEEP\n");
-        --sound_timer;
+            break;
     }
 }
 
+
 int main(int argc, char **argv) {
+    // load fontset
+    for (int i = 0; i < 80; i++)
+        memory[i] = fontset[i];
+
+    // load program
     FILE *f = fopen(argv[1], "rb");
     if (f == NULL) {
         printf("error; Couldn't open %s\n", argv[1]);
@@ -131,7 +202,19 @@ int main(int argc, char **argv) {
     fread(buffer, fsize, 1, f);
     fclose(f);
     for (int i = 0; i < fsize; i++)
-        printf("%04x\n", buffer[i]);
+        memory[i + 512] = buffer[i];
+
+    for (;;) {
+        emulateCycle();
+        // if (drawFlag)
+            // drax graphics
+        if (delay_timer > 0)
+            --delay_timer;
+        if (sound_timer > 0) {
+            printf("BEEP\n");
+            --sound_timer;
+        // store keys state
+    }
 
     return 0;
 }
