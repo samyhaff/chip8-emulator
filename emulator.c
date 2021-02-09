1552d4a622eaf68b70c57023f23dde0f548893d4
samyhaff <haffoudhisamy@gmail.com>
2021-02-09 09:53:29 +0100

    defined the chip8 struct

--- emulator.c
+++ emulator.c
@@ -2,4 +2,19 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+struct chip8 {
+    unsigned char memory[4096]; 
+    unsigned char V[16];
+    unsigned short I;
+    unsigned short pc;
+    unsigned char gfx[64 * 32];
+    unsigned char delay_timer;
+    unsigned char sound_timer;
+    unsigned short stack[16];
+    unsigned short sp;
+    unsigned char key[16];
+} chip8;
 
+int main() {
+    return 0;
+}
